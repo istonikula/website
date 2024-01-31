8e30411b11dae51f09c4d779258ed0381972a71d
Isto Nikula <isto.nikula@iki.fi>
2024-01-31 18:19:24 +0200

    home: generate skills from tags

---
+++ src/lib/tags.ts
@@ -0,0 +1,14 @@
+import { getCollection } from "astro:content";
+
+export async function allTags() {
+  const projects = await getCollection('projects')
+  const foss = await getCollection('foss')
+
+  const tags = [
+    ...new Set(
+      projects.map(x => x.data.tags).flat().concat(foss.map(x => x.data.tags).flat())
+    ),
+  ];
+
+  return tags
+}
