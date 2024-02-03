68dd62909b2abb3bcab5a186284315845c14b527
Isto Nikula <isto.nikula@iki.fi>
2024-02-03 11:29:49 +0200

    icons: use astro-icon instead of raw svg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
@@ -6,5 +7,15 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://istonikula.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [
+    icon({
+      include: {
+        heroicons: ['bars-3', 'chevron-down', 'home', 'swatch'],
+        mdi: ['github', 'linkedin']
+      }
+    }),
+    mdx(),
+    sitemap(),
+    tailwind()
+  ]
 });
